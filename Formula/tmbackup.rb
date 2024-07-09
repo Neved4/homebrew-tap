506d008c36b0eb50e4c57a36eb2b2c30d152e6f9
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2024-07-09 22:22:07 +0000

    tmbackup: add 0.1.0 bottle.

--- Formula/tmbackup.rb
+++ Formula/tmbackup.rb
@@ -12,6 +12,13 @@ class Tmbackup < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tmbackup-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "b75f4909b5fc829d2de0b6da036199480427abe428c2ae8f0045ef9753368ae2"
+    sha256 cellar: :any_skip_relocation, ventura:      "f9b0b53aa9d70e9f64f4d1b48c450b0e3d9cc164c241a623dde321117b7d78e2"
+    sha256 cellar: :any_skip_relocation, monterey:     "520088b0752a0c47ec08ed78f944f4d3b4ecc3544ea6e191824d30148c1330e7"
+  end
+
   depends_on :macos
 
   def install
