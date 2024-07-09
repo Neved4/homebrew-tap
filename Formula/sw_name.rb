c9b9562330b1096bf439768d8b70165f6e129739
risc <148492865+0risc@users.noreply.github.com>
2024-07-09 17:38:02 +0000

    sw_name: add 0.1.0 bottle.

--- Formula/sw_name.rb
+++ Formula/sw_name.rb
@@ -12,6 +12,12 @@ class SwName < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/sw_name-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "db9a53d1af449117fb2d26e691d359f6917ff9db51ca9a8022e965e546d03337"
+    sha256 cellar: :any_skip_relocation, ventura:      "eec1de1ca14568ea077bcb74049e002c1456db693da0ed68cc82402efff0375f"
+  end
+
   depends_on :macos
 
   def install
