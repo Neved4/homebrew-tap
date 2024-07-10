ef5718a6bb7b11cba29b0f3b2bd844859dc62b1c
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2024-07-10 08:37:41 +0000

    applist: add 0.1.0 bottle.

--- Formula/applist.rb
+++ Formula/applist.rb
@@ -11,6 +11,13 @@ class Applist < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/applist-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "08347646c55d0164c0b85f825c17767825b0c34535ced43a7ba870f6a74b1f04"
+    sha256 cellar: :any_skip_relocation, ventura:      "3e1728fac1160792d98fa884158dc55b865a716938794d2b164d1bddb608f455"
+    sha256 cellar: :any_skip_relocation, monterey:     "d78eb9cecd20b916dbaafaf90e4e2abb92c0c8558275de1543d3328163056240"
+  end
+
   depends_on :macos
 
   def install
