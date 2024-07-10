5da3f28f7546433174f67147bf745b786fe69904
risc <148492865+0risc@users.noreply.github.com>
2024-07-10 06:21:15 +0000

    Fix homepage
    Signed-off-by: risc <148492865+0risc@users.noreply.github.com>

--- Formula/applist.rb
+++ Formula/applist.rb
@@ -1,6 +1,6 @@
 class Applist < Formula
   desc "List app directories under macOS!"
-  homepage "https://github.com/Neved4/appdir"
+  homepage "https://github.com/Neved4/applist"
   url "https://github.com/Neved4/appdir/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "d247e41491a1552b73e4991e954772bea8002c51cc637e4b748153461589df62"
   license "MIT"
