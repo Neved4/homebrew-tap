9bb56dd11fd7c1c9a45280519cf40a636a7da15f
risc <148492865+0risc@users.noreply.github.com>
2024-07-12 05:24:46 +0000

    gotwc: add 1.1.1 bottle.

--- Formula/gotwc.rb
+++ Formula/gotwc.rb
@@ -11,6 +11,13 @@ class Gotwc < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/gotwc-1.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0b96aee46910236095b17dc66c18dd0ad01ff98e60df6e19fda0fb5d8e5476df"
+    sha256 cellar: :any_skip_relocation, ventura:      "850e542f3bb1597add8d88fcc27250d455e7d2643b52493b3907a02cc9ec5f38"
+    sha256 cellar: :any_skip_relocation, monterey:     "0b18edf50adcaed63c1d4cb9d1a46e8f6fa7749d3c0ebc557ce64ad4394120df"
+  end
+
   depends_on "go" => :build
 
   def install
