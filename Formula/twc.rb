60dab3cfb610df6571808b4eb1c97c90809d205e
risc <148492865+0risc@users.noreply.github.com>
2024-07-12 05:36:26 +0000

    twc: add 1.1.1 bottle.

--- Formula/twc.rb
+++ Formula/twc.rb
@@ -11,6 +11,13 @@ class Twc < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/twc-1.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1b825f603781decd62a82b2074a34de67dbe6265065dbc482650a3ded1797d7f"
+    sha256 cellar: :any_skip_relocation, ventura:      "472431c077235005e8531dc9f8fc32b930eb293d12b61842c6ee1657baa4f3a6"
+    sha256 cellar: :any_skip_relocation, monterey:     "6d3cc9d479f51ce1689c42957f1b0b2d3ad3c27402ce3d2548e0a62aa278c8e9"
+  end
+
   depends_on "gcc" => :build
 
   def install
