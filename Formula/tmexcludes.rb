f3d556c25d2fc9b16c6e8c80dadba87c1350581a
risc <148492865+0risc@users.noreply.github.com>
2024-07-12 16:35:39 +0000

    tmexcludes: add 0.1.0 bottle.

--- Formula/tmexcludes.rb
+++ Formula/tmexcludes.rb
@@ -11,6 +11,14 @@ class Tmexcludes < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tmexcludes-0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1bfa573e6e95ce4a9b06ba6da018a7d5aeec27331127d2c22d9acfff1ee39e26"
+    sha256 cellar: :any_skip_relocation, ventura:      "6a20f1fbc8e8df70768ebaa4c3f76c6cd6282457cb11b5854502f30da78a4469"
+    sha256 cellar: :any_skip_relocation, monterey:     "f004b7f71f9d51537276df6d66ac25bfb52f289f3ed53974c3d51150811689e4"
+  end
+
   depends_on :macos
 
   def install
