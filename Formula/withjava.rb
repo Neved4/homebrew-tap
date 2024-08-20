1e660876c39d1c98915225844feebd193ad9fd56
risc <148492865+0risc@users.noreply.github.com>
2024-08-20 16:50:45 +0000

    withjava: add 0.1.0-d71ed0dde95887e4d305f5bd6fd2f0e50079e436 bottle.

--- Formula/withjava.rb
+++ Formula/withjava.rb
@@ -13,6 +13,14 @@ class Withjava < Formula
     # regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/withjava-0.1.0-d71ed0dde95887e4d305f5bd6fd2f0e50079e436"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "2080409cbeb3890c4eeda79aae69c5c53b929bbc47a8e8aa0e7038b302b0d974"
+    sha256 cellar: :any_skip_relocation, ventura:      "ad46a3b6aa568cf48dba9a244b348cd5e74870cf74b1c97cbd434b04614fdb21"
+    sha256 cellar: :any_skip_relocation, monterey:     "302799d37d9fece35dd1c234ae87bdbea0e6c99f18692f4675fab5fe1e55eb86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bb969f32f130c43194f37cc6216c93ceb21b869809b8c76299404b0188464af7"
+  end
+
   depends_on "rust" => :build
 
   def install
