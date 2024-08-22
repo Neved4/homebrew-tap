81a4eacd011acd4f1f878e95065484b02ca50e51
risc <risc@fastmail.com>
2024-08-22 05:58:52 +0000

    Improve `doas` formula
    This test doesn't work. Not sure why.
    
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/doas.rb
+++ Formula/doas.rb
@@ -1,27 +1,29 @@
 class Doas < Formula
   desc "Execute commands as another user"
   homepage "https://github.com/slicer69/doas"
-  url "https://github.com/slicer69/doas/releases/tag/6.3p12"
-  sha256 "f75577d7280a5596457aac9bb0c672cb9e75d0cb2222950dc703622335330f5a"
+  url "https://github.com/slicer69/doas/archive/refs/tags/6.3p12.tar.gz"
+  version "6.3p12"
+  sha256 "e4f37745345c12d4e0c8c03c8237791729cf047dbd7b2455f8de60e2f82ac1b0"
   license "BSD-2-Clause"
   head "https://github.com/slicer69/doas.git", branch: "master"
 
   livecheck do
     url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
-
-  depends_on "byacc" => :build
 
   def install
-    system "ln", "-s", "/opt/homebrew/bin/byacc", "/opt/homebrew/bin/yacc"
-    system "export", "PATH=/opt/homebrew/bin/yacc:$PATH"
     system "make", "all"
-    system "make", "install"
+    bin.install "doas"
+    man1.install "doas.1"
+    man1.install "doas.conf.5"
+    bin.install "doasedit"
+    man1.install "doasedit.8"
+    bin.install "vidoas"
+    man1.install "vidoas.8"
   end
 
-  def post_install
-    system "make", "clean"
-    system "rm", "/opt/homebrew/bin/yacc"
+  test do
+    desired_output = "usage: doas"
+    assert_includes shell_output("#{bin}/doasedit").strip, desired_output
   end
 end
