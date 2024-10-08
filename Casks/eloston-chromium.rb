d9852a0c0ebf80a7e984bfb574b3a6683668062c
risc <risc@fastmail.com>
2024-10-08 16:52:15 +0000

    Remove `conflicts_with` block
    Signed-off-by: risc <risc@fastmail.com>

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -22,10 +22,6 @@ cask "eloston-chromium" do
     end
   end
 
-  conflicts_with cask: [
-    "chromium",
-    "freesmug-chromium",
-  ]
   depends_on macos: ">= :big_sur"
 
   app "Chromium.app", target: "Chromium Ungoogled.app"
