720c3321697beb277b90eeb7d829aef9983d09e3
risc <risc@fastmail.com>
2024-10-24 06:54:12 +0000

    nomore403 1.0.2

--- Formula/nomore403.rb
+++ Formula/nomore403.rb
@@ -1,8 +1,8 @@
 class Nomore403 < Formula
   desc "Tool to bypass 403/40X response codes"
   homepage "https://github.com/devploit/nomore403"
-  url "https://github.com/devploit/nomore403/archive/refs/tags/1.0.1.tar.gz"
-  sha256 "0cdb557ac0515d1b5580f5bcb6045800c13c9868e3f3e8dafcf15f5bc253d013"
+  url "https://github.com/devploit/nomore403/archive/refs/tags/1.0.2.tar.gz"
+  sha256 "1fda89e455b1372e718a6028e9576cd8cc5206524897b7a63c5fbe0448832cdf"
   license "MIT"
   head "https://github.com/devploit/nomore403.git", branch: "main"
 
