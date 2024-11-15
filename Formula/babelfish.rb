c5d492c68bf7ac15313490f2a0c5079465a330c3
risc <148492865+0risc@users.noreply.github.com>
2024-11-15 17:45:24 +0000

    babelfish: add 1.2.1 bottle.

--- Formula/babelfish.rb
+++ Formula/babelfish.rb
@@ -6,6 +6,14 @@ class Babelfish < Formula
   license "MIT"
   head "https://github.com/bouk/babelfish.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/babelfish-1.2.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f613208c8f9c9e877ddaa3fe3c51b7cf63712813910fe3260d67e15535bdb552"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4eab6ed062985b36e62b2804b57d94767802299c4b3ce740e7aea08d1836a340"
+    sha256 cellar: :any_skip_relocation, ventura:       "79cec3f8a1587dcd122ef6a00e2324ed31223865c83faae589d18dcf24d02e53"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f639a94755a91f0589c575ec10fcbf7c5c0430e67f648963b765cfd532f09ab"
+  end
+
   depends_on "go" => :build
   depends_on "fish" => :test
 
