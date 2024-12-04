b555d46a2763171f661a8ec7d3fa31e5836c35b0
risc <risc@fastmail.com>
2024-12-04 09:10:25 -0600

    Update Casks/font-monego.rb
    Closes #103.
    
    Co-authored-by: ⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
    Signed-off-by: ⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>

--- Casks/font-monego.rb
+++ Casks/font-monego.rb
@@ -1,8 +1,11 @@
+# typed: strict
+# frozen_string_literal: true
+
 cask "font-monego" do
   version "0.0.0,ad2acdcfc48277dabdededd3a46f1348f720d110"
   sha256 "62a0135b020858a024736b6b3415ae5f37240c31cca138b849f4a906e12c69da"
 
-  url "https://github.com/cseelus/monego/archive/#{version.csv.second}.tar.gz"
+  url "https://github.com/cseelus/monego/archive/refs/heads/#{version.csv.second}.tar.gz"
   name "Monego"
   desc "Beloved Monaco monospaced font, recreated with bold and italic variants"
   homepage "https://github.com/cseelus/monego"
@@ -15,12 +18,10 @@ cask "font-monego" do
   font "monego-#{version.csv.second}/Monego/Monego-BoldItalic.otf"
   font "monego-#{version.csv.second}/Monego/Monego-Italic.otf"
   font "monego-#{version.csv.second}/Monego/Monego-Regular.otf"
-  # Monego with ligatures
   font "monego-#{version.csv.second}/Monego-with-ligatures/MonegoLigatures-Bold.otf"
   font "monego-#{version.csv.second}/Monego-with-ligatures/MonegoLigatures-BoldItalic.otf"
   font "monego-#{version.csv.second}/Monego-with-ligatures/MonegoLigatures-Italic.otf"
   font "monego-#{version.csv.second}/Monego-with-ligatures/MonegoLigatures-Regular.otf"
-  # Monego with ligatures and nerd font glyphs
   font "monego-#{version.csv.second}/Monego-Nerd-Font/Monego_Nerd_Fixed-Bold.otf"
   font "monego-#{version.csv.second}/Monego-Nerd-Font/Monego_Nerd_Fixed-BoldItalic.otf"
   font "monego-#{version.csv.second}/Monego-Nerd-Font/Monego_Nerd_Fixed-Italic.otf"
