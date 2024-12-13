9da7627c24f0d1eca470e580fa1110c78a3aa807
risc <risc@fastmail.com>
2024-12-13 18:02:08 +0000

    nomore403: Update to current unstable version, install `payloads` folder, and add test
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/nomore403.rb
+++ Formula/nomore403.rb
@@ -1,10 +1,15 @@
 class Nomore403 < Formula
   desc "Tool to bypass 403/40X response codes"
   homepage "https://github.com/devploit/nomore403"
-  url "https://github.com/devploit/nomore403/archive/refs/tags/1.0.2.tar.gz"
-  sha256 "1fda89e455b1372e718a6028e9576cd8cc5206524897b7a63c5fbe0448832cdf"
+  url "https://github.com/devploit/nomore403/archive/a47e83b54def7537fe9456e6a327fef1c32939b0.tar.gz"
+  version "1.0.2-unstable-001,a47e83b54def7537fe9456e6a327fef1c32939b0"
+  sha256 "72e79b75857af47a4685cf50a91aa402a9c82e004b55a0f79fe0cdc6e2fa7a79"
   license "MIT"
   head "https://github.com/devploit/nomore403.git", branch: "main"
+
+  livecheck do
+    skip "Current version is broken"
+  end
 
   bottle do
     root_url "https://github.com/Neved4/homebrew-tap/releases/download/nomore403-1.0.2"
@@ -15,7 +20,7 @@ class Nomore403 < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "ff3ad4084fc1718bd5b5d3a896a3c705456b35b925bb154915de1166616a149a"
   end
 
-  depends_on "go" => :build
+  depends_on "go" => [:build, :test]
 
   def install
     system "go", "build",
@@ -23,6 +28,48 @@ class Nomore403 < Formula
         ldflags: "-w -s",
         gcflags: "all=-l -B -wb=false",
       )
+    bin.install "payloads"
     doc.install "README.md"
+  end
+
+  def caveats
+    <<~CAVEATS
+      You must provide the path to a payloads folder with -f or --folder. The default payloads
+      folder has been installed to #{bin}/payloads. Example:
+
+        $ nomore403 -f #{bin}/payloads -u https://example.com
+    CAVEATS
+  end
+
+  test do
+    (testpath/"403.go").write <<~GO
+      package main
+      import ("fmt"; "io"; "net/http")
+      func status(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(403)
+        io.WriteString(w, "Error 403 Forbidden")
+        fmt.Println("User agent:", r.UserAgent())
+      }
+      func main() {
+        http.HandleFunc("/", status)
+        http.ListenAndServe(":8000", nil)
+      }
+    GO
+
+    system "go", "build", "403.go"
+
+    begin
+      pid = spawn "#{testpath}/403 > log.txt"
+      sleep 1
+      desired_output_nomore403 = "162 bytes"
+      command = "#{bin}/nomore403 -f #{bin}/payloads -k verbs -u http://localhost:8000"
+      assert_includes shell_output(command).strip, desired_output_nomore403
+
+      desired_output_logs = "User agent: nomore403"
+      assert_includes shell_output("cat #{testpath}/log.txt").strip, desired_output_logs
+    ensure
+      Process.kill("SIGTERM", pid)
+      Process.wait(pid)
+    end
   end
 end
