ebfff8d546e99cccf547f53a19803f6efa295a82
risc <148492865+0risc@users.noreply.github.com>
2024-12-21 16:51:06 +0000

    johnnybgoode: add 1.1.0 bottle.

--- Formula/johnnybgoode.rb
+++ Formula/johnnybgoode.rb
@@ -6,6 +6,14 @@ class Johnnybgoode < Formula
   license "MIT"
   head "https://github.com/SwissArmyWrench/johnnybgoode.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/johnnybgoode-1.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "203f5b3102d3636b7f0cf50fee3b72243190fe89d6914e31c767b6923c8f84f3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ba1f63f0d08040e2780e7eb829cf8fe6fb6940774ed06fb47e928da33c972c2"
+    sha256 cellar: :any_skip_relocation, ventura:       "0e3f0991ac6513c997cc0ba70a72206cc26a4f3f6f96081f916038416b9ee6a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3614074db0fc7a1d2feeee7052e7bfc0e81b899d24e75efd8ac17d8955ca050"
+  end
+
   depends_on "rust" => :build
 
   def install
