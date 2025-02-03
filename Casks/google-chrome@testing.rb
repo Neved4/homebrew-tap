357f382e17e99c4e885f19e8d00526fe1bd8df90
Neved4 <63655535+Neved4@users.noreply.github.com>
2025-02-04 00:12:18 +0100

    Fix `chrome-testing.rb`
    Closes #130.
    
    Signed-off-by: ⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>

--- Casks/google-chrome@testing.rb
+++ Casks/google-chrome@testing.rb
@@ -1,8 +1,10 @@
-cask "chrome-testing" do
+cask "google-chrome@testing" do
+  arch arm: "arm64", intel: "x64"
+
   version "132.0.6834.159"
   sha256 :no_check
 
-  url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-x64/chrome-mac-x64.zip",
+  url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
       verified: "storage.googleapis.com/chrome-for-testing-public/"
   name "Google Chrome for Testing"
   desc "Official Chrome build for testing automation"
@@ -19,8 +21,7 @@ cask "chrome-testing" do
   auto_updates false
   depends_on macos: ">= :big_sur"
 
-  binary "chrome-mac-x64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome",
-         target: "chrome-testing"
+  app "chrome-mac-#{arch}/Google Chrome for Testing.app"
 
   zap trash: [
         "~/Library/Application Support/Google/Chrome for Testing",
