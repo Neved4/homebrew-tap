fe35e7535ea082a095da6ad41d343c1405d3d680
risc <risc@fastmail.com>
2025-02-03 08:29:34 +0000

    Fix url and livecheck
    Signed-off-by: risc <risc@fastmail.com>

--- Casks/chromium-stable.rb
+++ Casks/chromium-stable.rb
@@ -1,41 +1,39 @@
 cask "chromium-stable" do
-  arch = { arm: "MacOsArm64", intel: "MacOsx" }
+  arch arm: "MacOsArm64", intel: "MacOsx"
 
   version "2024.12.1"
   sha256 arm:   "969368a379fdaa3995dda410230a1b11f8189ab243572900dbecb8d0d0c82725",
          intel: "3d9cec0009285e5c9892537985e3b510d306ee6f2414b3980c4a4f2a794c21af"
 
-  on_arm do
-    url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=MacOsArm64",
-        verified: "portswigger-cdn.net/burp/releases/"
-  end
-  on_intel do
-    url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=MacOsx",
-        verified: "portswigger-cdn.net/burp/releases/"
-  end
-
+  url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
+      verified: "portswigger-cdn.net/burp/releases/"
   name "Burp Chromium"
   desc "Chromium browser extracted from Burp Suite Community Edition"
   homepage "https://portswigger.net/burp/"
 
   livecheck do
+    url "https://portswigger.net/burp/releases/data"
     strategy :json do |json|
-      json.dig("ResultSet", "Results")&.filter_map do |item|
-        if item["releaseChannels"]&.include?("Stable") &&
-           item["categories"]&.include?("Community") &&
-           item["builds"]&.any? do |build|
-             build["ProductPlatform"] == (on_arm do
-               "MacOsArm64"
-             end
-                                          on_intel do
-                                            "MacOsx"
-                                          end)
-           end
-          item["version"]
-        end
+      all_versions = json.dig("ResultSet", "Results")
+      next if all_versions.blank?
+
+      all_versions.filter_map do |item|
+        item["version"] if
+              item["releaseChannels"]&.include?("Stable") &&
+              item["categories"]&.include?("Community") &&
+              item["builds"]&.any? do |build|
+                build["ProductPlatform"] == arch.to_s
+              end
       end
     end
   end
+
+  conflicts_with cask: [
+    "eloston-chromium",
+    "freesmug-chromium",
+    "chromium-enabled",
+    "chromium-ungoogled",
+  ]
 
   postflight do
     burp_app = staged_path.join("Burp Suite Community Edition.app")
