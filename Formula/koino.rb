d4cabaf23649f0e77fb584d6febdc5598d256d1a
risc <risc@fastmail.com>
2025-02-09 01:13:00 +0000

    Remove option to build `koino` from head
    `brew audit` complains because branch `zig-0.13.0` is not the default branch but the default branch won't work with zig >=0.13.0.
    
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/koino.rb
+++ Formula/koino.rb
@@ -4,7 +4,8 @@ class Koino < Formula
   url "https://github.com/kivikakk/koino/archive/0151bb37714d93688f31e3d7a3d0369106818f26.tar.gz"
   sha256 "3f98f9eb38350dab8fd3718df26e7cef2a96caf30f2baf12aa62f2c773fecf2a"
   license "MIT"
-  head "https://github.com/kivikakk/koino.git", branch: "zig-0.13.0"
+  # brew wants `main` which won't actually build on zig >=0.13.0
+  #head "https://github.com/kivikakk/koino.git", branch: "zig-0.13.0"
 
   livecheck do
     skip "No tagged version available"
