e29f497abd612df7f753042c05b22c8a468834b4
risc <148492865+0risc@users.noreply.github.com>
2025-02-09 17:56:24 +0000

    koino: add 26 bottle.

--- Formula/koino.rb
+++ Formula/koino.rb
@@ -11,6 +11,14 @@ class Koino < Formula
     skip "No tagged version available"
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/koino-26"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8d33701e7353be5037ab39cf40d4b7d98c4371e056024bd1c3f7b9b92d46e44"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9235cb4dcc828434d876064458a6794fb5e34a4e7c4259b99be0dce60e3c6cfa"
+    sha256 cellar: :any_skip_relocation, ventura:       "a9525221b18824f69312a07bc6a1dc41a71e9ba6ec62e5a4f1fc1f0752e73b70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93243b5eafe2cff49f15bc67a0bb6eff39a8c30fd95e9f34fbd00fa15848c54c"
+  end
+
   depends_on "zig" => :build
 
   def install
