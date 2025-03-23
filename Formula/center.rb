3bf9f4056316b10a3f2685381c508de71b12f7de
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-03-23 00:47:39 +0000

    center: add 0.1.0-239545081facf055a4928ee5fb947150ebbd5e65 bottle.

--- Formula/center.rb
+++ Formula/center.rb
@@ -11,6 +11,14 @@ class Center < Formula
     skip "No tagged version available"
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/center-0.1.0-239545081facf055a4928ee5fb947150ebbd5e65"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "61ce46e776320769221b1df086312cda9efd69e5e5385a5e3cbba08192e23d64"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2fb760d99e8fc3ee206940e892f20fa16f3e804b739fedd2e3dff8a67789971e"
+    sha256 cellar: :any_skip_relocation, ventura:       "3e423bfc52be2face3752c330e59b6e336d702b5d28753b5a1fdb2b326881aa1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "77ae709ddf12f031d6d0ee7fc26ba3d5bd14e8f62cb5fa7f4a4324c95ea6168b"
+  end
+
   def install
     system ENV.cc, "-O3", "center.c", "-o", "center"
     bin.install "center"
