60801a8d31fc576950ddc304c526cb37bef20c12
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-04-18 00:36:53 +0000

    chromium@stable 2025.3.2

--- Casks/chromium@stable.rb
+++ Casks/chromium@stable.rb
@@ -1,7 +1,7 @@
 cask "chromium@stable" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2025.2.4"
+  version "2025.3.2"
   sha256 :no_check
 
   url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
