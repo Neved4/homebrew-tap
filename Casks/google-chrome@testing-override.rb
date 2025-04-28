bb2d9e7c7af4501f29199bb9586b057683abe505
risc <risc@fastmail.com>
2025-04-28 07:08:44 +0000

    Add `google-chrome@testing-override` cask
    Closes #204.
    
    Signed-off-by: risc <risc@fastmail.com>
    Signed-off-by: ⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>

---
+++ Casks/google-chrome@testing-override.rb
@@ -0,0 +1,43 @@
+cask "google-chrome@testing-override" do
+  arch arm: "arm64", intel: "x64"
+
+  version "135.0.7049.114"
+  sha256 :no_check
+
+  url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
+      verified: "storage.googleapis.com/chrome-for-testing-public/"
+  name "Google Chrome for Testing"
+  desc "Official Chrome build for testing automation"
+  homepage "https://googlechromelabs.github.io/chrome-for-testing/"
+
+  livecheck do
+    url "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json"
+    strategy :page_match do |page|
+      data = JSON.parse(page)
+      data.dig("channels", "Stable", "version")
+    end
+  end
+
+  auto_updates false
+  conflicts_with cask: "google-chrome"
+  depends_on macos: ">= :big_sur"
+
+  app "chrome-mac-#{arch}/Google Chrome for Testing.app", target: "Google Chrome.app"
+
+  preflight do
+    FileUtils.cp("Resources/google-chrome.icns",
+                 "#{staged_path}/chrome-mac-#{arch}/Google Chrome for Testing.app/Contents/Resources/app.icns")
+  end
+
+  zap trash: [
+        "~/Library/Application Support/Google/Chrome for Testing",
+        "~/Library/Caches/com.google.ChromeTesting",
+        "~/Library/Preferences/com.google.ChromeTesting.plist",
+        "~/Library/Saved Application State/com.google.ChromeTesting.savedState",
+      ],
+      rmdir: [
+        "~/Library/Application Support/Google/Chrome for Testing",
+        "~/Library/Caches/Google",
+        "~/Library/Google",
+      ]
+end
