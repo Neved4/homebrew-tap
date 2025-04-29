77b3c49fff88145087d9e92401dea344d5ff2f4b
risc <risc@fastmail.com>
2025-04-29 12:20:36 +0000

    Remove bottle
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/projectdiscovery-urlfinder.rb
+++ Formula/projectdiscovery-urlfinder.rb
@@ -6,14 +6,6 @@ class ProjectdiscoveryUrlfinder < Formula
   license "MIT"
   head "https://github.com/projectdiscovery/urlfinder.git", branch: "dev"
 
-  bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/urlfinder-0.0.3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dddd807a494d185db043b3faef7ef1d61b5dc0682e03f4261617c0ab10557c96"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "191826bd4a9a4b2f80158ae3912576f8490cd80e341cf797dc4815f2dbddc40d"
-    sha256 cellar: :any_skip_relocation, ventura:       "7263ce15d5c4745c562f5a9f55a197a8a050e9adf4db0b82eb4521528126cbf4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8d5720c8bc7c07c26adc3a0aa40e1717ff74ad89306bea968560c488452a97a"
-  end
-
   depends_on "go" => :build
 
   def install
