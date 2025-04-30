26911e8c522f97329e04606ef1b3b391c58d5429
risc <risc@fastmail.com>
2025-04-30 10:17:49 +0000

    Rename to `pd-urlfinder`
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/pd-urlfinder.rb
+++ Formula/pd-urlfinder.rb
@@ -1,4 +1,4 @@
-class ProjectdiscoveryUrlfinder < Formula
+class PdUrlfinder < Formula
   desc "Tool for passively gathering URLs"
   homepage "https://github.com/projectdiscovery/urlfinder"
   url "https://github.com/projectdiscovery/urlfinder/archive/refs/tags/v0.0.3.tar.gz"
@@ -22,7 +22,7 @@ class ProjectdiscoveryUrlfinder < Formula
 
     # very hacky workaround for https://github.com/projectdiscovery/urlfinder/issues/113
     10.times do |i|
-      output = pipe_output("#{bin}/projectdiscovery-urlfinder --version 2>&1").strip
+      output = pipe_output("#{bin}/pd-urlfinder --version 2>&1").strip
       ohai "Attempt #{i}"
       if output.include?(desired_output)
         assert_includes output, desired_output
