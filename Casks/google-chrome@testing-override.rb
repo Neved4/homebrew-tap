348d2558f73090239d12d0d14d694ee2d09d84ac
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-05-28 11:32:29 +0000

    google-chrome@testing-override 137.0.7151.55
    Closes #233.
    
    Signed-off-by: ⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>

--- Casks/google-chrome@testing-override.rb
+++ Casks/google-chrome@testing-override.rb
@@ -1,7 +1,7 @@
 cask "google-chrome@testing-override" do
   arch arm: "arm64", intel: "x64"
 
-  version "136.0.7103.113"
+  version "137.0.7151.55"
   sha256 :no_check
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
