300c540492d0aae1504b4c594b07694f1cac58e0
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-06-01 02:31:48 +0000

    chromium@ungoogled 137.0.7151.55-1.1

--- Casks/chromium@ungoogled.rb
+++ Casks/chromium@ungoogled.rb
@@ -1,7 +1,7 @@
 cask "chromium@ungoogled" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "136.0.7103.113-1.1"
+  version "137.0.7151.55-1.1"
   sha256 :no_check
 
   url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_#{arch}-macos.dmg",
