25acc9f4b9de77527bc5d26e6f65902e6e47dcdc
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-06-05 20:25:21 +0000

    google-chrome@testing-override 137.0.7151.68

--- Casks/google-chrome@testing-override.rb
+++ Casks/google-chrome@testing-override.rb
@@ -1,7 +1,7 @@
 cask "google-chrome@testing-override" do
   arch arm: "arm64", intel: "x64"
 
-  version "137.0.7151.55"
+  version "137.0.7151.68"
   sha256 :no_check
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
