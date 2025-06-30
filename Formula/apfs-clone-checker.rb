5e04fbce7c87a196d24c56f5ccd092429678bfe0
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-06-30 18:34:34 +0000

    apfs-clone-checker: add 1.0.0.0 bottle.

--- Formula/apfs-clone-checker.rb
+++ Formula/apfs-clone-checker.rb
@@ -6,6 +6,13 @@ class ApfsCloneChecker < Formula
   license "MIT"
 
   head "https://github.com/dyorgio/apfs-clone-checker.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/apfs-clone-checker-1.0.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf333df891b0c73e5cdc22e8bdb3806f44764ccef4aded9c17037483718289e5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b1389bd3e5bb50ac7a4f898658439f452f29d39bfefbe47b72e13d0a51d21837"
+    sha256 cellar: :any_skip_relocation, ventura:       "8c4dfb2ada69ab8b0c852e875c1b5501998d909467478e62da76d602c49501ca"
+  end
   depends_on :macos
 
   def install
