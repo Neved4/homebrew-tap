cac6b392748c46befaa878aff98af0444b615395
risc <risc@fastmail.com>
2025-07-27 18:16:55 +0000

    wordtidy: try removing bottles
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/wordtidy.rb
+++ Formula/wordtidy.rb
@@ -6,14 +6,6 @@ class Wordtidy < Formula
   license "MIT"
   head "https://github.com/sts10/tidy.git", branch: "main"
 
-  bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/wordtidy-0.3.16"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47f9115d69fb54fdbf7aed7577cbca0b8fd1ed6bd77d0cee85ec5202d74763be"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "890ebce5a557964d96d08fcea1cae9fd83a4b93e06507f07c32b5a3b8f2c1559"
-    sha256 cellar: :any_skip_relocation, ventura:       "7ab1411a3e5c4c4c0ba0f3bd904b85fd662e18b61b645bf4c6e850cff35517c5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9da8ed45cf5fa438deb57e7e06d923a3e223a209f4ff90ebab4a04a99650393"
-  end
-
   depends_on "rust" => :build
 
   def install
