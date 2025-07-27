dc1055185402fe0332a66944001fa20de416282c
Neved4 <63655535+Neved4@users.noreply.github.com>
2025-07-27 17:57:40 +0000

    wordtidy 0.3.17

--- Formula/wordtidy.rb
+++ Formula/wordtidy.rb
@@ -1,8 +1,8 @@
 class Wordtidy < Formula
   desc "Combine and clean word lists"
   homepage "https://github.com/sts10/tidy"
-  url "https://github.com/sts10/tidy/archive/refs/tags/v0.3.16.tar.gz"
-  sha256 "617a866fa90176d6610247ba13da0605b674fe0c8fc7fe41db0610977d23dc31"
+  url "https://github.com/sts10/tidy/archive/refs/tags/v0.3.17.tar.gz"
+  sha256 "1e34de32b679b37efdd1986f85a5d466cdb62ee9e52033d1a6acbde03efd5c6d"
   license "MIT"
   head "https://github.com/sts10/tidy.git", branch: "main"
 
