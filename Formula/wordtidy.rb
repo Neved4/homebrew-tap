819a1d3388f3dbe13c87aeb9a3180e730a388141
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-08-03 23:45:59 +0000

    wordtidy: add 0.3.17 bottle.

--- Formula/wordtidy.rb
+++ Formula/wordtidy.rb
@@ -6,6 +6,14 @@ class Wordtidy < Formula
   license "MIT"
   head "https://github.com/sts10/tidy.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/wordtidy-0.3.17"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "50aca078d786dc1918ed3bb0458543390ce6a41f8f834918ca3b84dec3c455d5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f31df42a77a93dd402f476c3e4a6ee2d3ad148f7d84108151d451c42d2e17bf8"
+    sha256 cellar: :any_skip_relocation, ventura:       "3b069f1de924a54cefe5dab52275ae112a5bbdee0dce06afc9a344e1060f4bcd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70553c0c0be841c75a97d484740de151f907d996d9a142008af7c42ebba3060f"
+  end
+
   depends_on "rust" => :build
 
   def install
