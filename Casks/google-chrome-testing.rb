a27db36773595c9bf11a253707d4c3f349f6ae18
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-08-05 20:36:09 +0000

    google-chrome-testing 139.0.7258.66
    Closes #273.
    
    Signed-off-by: ⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>

--- Casks/google-chrome-testing.rb
+++ Casks/google-chrome-testing.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-testing" do
   arch arm: "arm64", intel: "x64"
 
-  version "138.0.7204.183"
+  version "139.0.7258.66"
   sha256 :no_check
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
