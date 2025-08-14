6114821736bc04227f350a5fb0eceff6f3b78375
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-08-14 17:28:40 +0000

    burp-browser 2025.7.3

--- Casks/burp-browser.rb
+++ Casks/burp-browser.rb
@@ -1,7 +1,7 @@
 cask "burp-browser" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2025.7.2"
+  version "2025.7.3"
   sha256 :no_check
 
   url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
