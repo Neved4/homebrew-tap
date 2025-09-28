7ff002338a17f6596fcc65f207ba21810eab3dcd
⑆ Neveda ⑈ <63655535+Neved4@users.noreply.github.com>
2025-09-28 10:37:21 +0000

    burp-browser 2025.8.6

--- Casks/burp-browser.rb
+++ Casks/burp-browser.rb
@@ -1,7 +1,7 @@
 cask "burp-browser" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2025.8.5"
+  version "2025.8.6"
   sha256 :no_check
 
   url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
