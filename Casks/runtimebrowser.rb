4cdc4707ef3b27c34ceaca7b5b341433ebc341ef
risc <risc@fastmail.com>
2025-10-25 18:28:51 +0000

    runtimebrowser: Fix URL verification, add caveat
    Signed-off-by: risc <risc@fastmail.com>

--- Casks/runtimebrowser.rb
+++ Casks/runtimebrowser.rb
@@ -2,8 +2,8 @@ cask "runtimebrowser" do
   version "0.996"
   sha256 "917fa8e5e5ee4d8bf580fa9eb7dea67087986a557c5b1ece7ce5e703b8f9a829"
 
-  # seriot.ch was verified as official
   url "http://seriot.ch/temp/RuntimeBrowser_#{version}.zip"
+    verified: "seriot.ch/"
   name "RuntimeBrowser"
   desc "Class browser for the Objective-C runtime"
   homepage "https://github.com/nst/RuntimeBrowser"
@@ -15,4 +15,8 @@ cask "runtimebrowser" do
   app "RuntimeBrowser.app"
 
   zap trash: "~/Library/Preferences/ch.seriot.RuntimeBrowser.plist"
+
+  caveats do
+    requires_rosetta
+  end
 end
