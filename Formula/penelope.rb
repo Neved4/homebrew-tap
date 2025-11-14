db6dd501942e1f6a289e228d0db71cdb53bcad65
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2025-11-14 16:00:11 +0000

    penelope: add 0.14.8 bottle.

--- Formula/penelope.rb
+++ Formula/penelope.rb
@@ -6,6 +6,13 @@ class Penelope < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/brightio/penelope.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/penelope-0.14.8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "436c8d1a5ffce6144267e64a7ab9f2586f2f2e78acb99ce132874968c89366db"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b49944a8973ddcd87661cf91990334295d79b4c192baa38665f965d182cce15b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "882d68f05cf0b98adbda893b58224040426fabfa184cf5ce59c9d4197a2b0267"
+  end
+
   depends_on "python@3.14"
 
   def install
