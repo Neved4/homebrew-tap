de2e68c232195545ea26548c517ae03d477d03b7
Neved4 <63655535+Neved4@users.noreply.github.com>
2025-11-23 15:55:03 +0100

    nbtscan: add formula

---
+++ Formula/nbtscan.rb
@@ -0,0 +1,24 @@
+class Nbtscan < Formula
+	desc "Scan networks for NetBIOS name information"
+	homepage "https://salsa.debian.org/pkg-security-team/nbtscan"
+	url "https://salsa.debian.org/pkg-security-team/nbtscan/-/archive/upstream/" \
+			"1.7.2/nbtscan-upstream-1.7.2.tar.gz"
+	sha256 "2adad7f157e036d5a4d175650854c1233409e371d4b796c52ae3c29d5599bb61"
+	license "GPL-2.0-or-later"
+	head "https://salsa.debian.org/pkg-security-team/nbtscan.git",
+		branch: "upstream"
+
+	depends_on "autoconf" => :build
+	depends_on "automake" => :build
+
+	def install
+		system "./autogen.sh"
+		system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+		system "make", "install"
+	end
+
+	test do
+		versionOutput = shell_output("#{bin}/nbtscan -V").strip
+		assert_match version.to_s, versionOutput
+	end
+end
