fbc2bd8307252160f432b93a87288a61e16b2a05
Neved4 <63655535+Neved4@users.noreply.github.com>
2025-11-25 21:59:21 +0100

    dnspeek 0.1.0

--- Formula/dnspeek.rb
+++ Formula/dnspeek.rb
@@ -1,21 +1,15 @@
 class Dnspeek < Formula
   desc "DNS reconnaissance CLI written in Go"
   homepage "https://github.com/Neved4/dnspeek"
-  url "https://github.com/Neved4/dnspeek/archive/a7ed8a43bad68a8c96036dcb6eac95cc8ba4170e.tar.gz"
-  version "0.0.0_a7ed8a43bad68a8c96036dcb6eac95cc8ba4170e"
-  sha256 "317cdd4bf9a18c1e67468608ffeb4690adab39a2883a87c61cee1c8f33d8ccd2"
+  url "https://github.com/Neved4/dnspeek/archive/" \
+      "062617e4a4866861d7c1b521f2ff354a3d88d0d4.tar.gz"
+  version "0.0.0_062617e4a4866861d7c1b521f2ff354a3d88d0d4"
+  sha256 "e3da47f61faeaf16eac95beacc305339d2a79513e84cbfa1e28c8d05fc64c04d"
   license "MIT"
   head "https://github.com/Neved4/dnspeek.git", branch: "main"
 
   livecheck do
     skip "No tagged version available"
-  end
-
-  bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/dnspeek-0.0.0_a7ed8a43bad68a8c96036dcb6eac95cc8ba4170e"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "788b086537010c2305977606dca702759f0cf37ad840ce332447cba659e2e7ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6f07a704b1e3c9dd57b529281b1effa178e1212ba7a72547760e20a4b881c01"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6b1c1ea7a585190a680910bf36dd18a0dea852bca11bec9563c0d549af43a30"
   end
 
   depends_on "go" => :build
@@ -35,6 +29,6 @@ class Dnspeek < Formula
 
   test do
     help_output = shell_output("#{bin}/dnspeek --help")
-    assert_includes help_output, "dnspeek - a cozy Go rewrite of dnsrecon"
+    assert_match "usage: dnspeek", help_output
   end
 end
