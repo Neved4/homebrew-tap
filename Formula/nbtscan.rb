dd8e45149e504a4c4750aa5c23cf9c4dc675255c
risc <risc@fastmail.com>
2025-11-26 08:07:50 +0000

    fix(nbtscan): fix URL, test
    Signed-off-by: risc <risc@fastmail.com>

--- Formula/nbtscan.rb
+++ Formula/nbtscan.rb
@@ -1,12 +1,12 @@
 class Nbtscan < Formula
   desc "Scan networks for NetBIOS name information"
-  homepage "https://salsa.debian.org/pkg-security-team/nbtscan"
-  url "https://salsa.debian.org/pkg-security-team/nbtscan/-/archive/upstream/" \
-      "1.7.2/nbtscan-upstream-1.7.2.tar.gz"
+  homepage "https://github.com/resurrecting-open-source-projects/nbtscan"
+  url "https://github.com/resurrecting-open-source-projects/nbtscan/" \
+      "archive/refs/tags/1.7.2.tar.gz"
   sha256 "2adad7f157e036d5a4d175650854c1233409e371d4b796c52ae3c29d5599bb61"
   license "GPL-2.0-or-later"
-  head "https://salsa.debian.org/pkg-security-team/nbtscan.git",
-    branch: "upstream"
+  head "https://github.com/resurrecting-open-source-projects/nbtscan.git",
+    branch: "master"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -18,7 +18,7 @@ class Nbtscan < Formula
   end
 
   test do
-    version_output = shell_output("#{bin}/nbtscan --help").strip
+    version_output = shell_output(bin/"nbtscan").strip
     assert_match "NBTscan version #{version}", version_output
   end
 end
