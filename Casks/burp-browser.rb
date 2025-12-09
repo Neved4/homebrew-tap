61216dec38f60e034f19193354c13c8d6e2973b9
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2025-12-09 09:34:18 +0000

    burp-browser 2025.10.7

--- Casks/burp-browser.rb
+++ Casks/burp-browser.rb
@@ -1,7 +1,7 @@
 cask "burp-browser" do
   arch arm: "MacOsArm64", intel: "MacOsx"
 
-  version "2025.10.6"
+  version "2025.10.7"
   sha256 :no_check
 
   url "https://portswigger-cdn.net/burp/releases/download?product=community&version=#{version}&type=#{arch}",
