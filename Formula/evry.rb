8de539541f9520604433cfb8f36f148bf90c140d
risc <risc@fastmail.com>
2025-12-10 06:26:06 +0000

    fix(formula): change version number format
    This may fix the Linux build which failed with
    
    ```
    error: linking with `cc` failed: exit status: 1
    ```
    ...
    ```
    	= note: /home/linuxbrew/.linuxbrew/opt/binutils/bin/ld: cannot find 1: No such file or directory
                /home/linuxbrew/.linuxbrew/opt/binutils/bin/ld: cannot find 5e3ebfb/lib: No such file or directory
                collect2: error: ld returned 1 exit status
    ```
    
    Closes #341.
    
    Signed-off-by: risc <risc@fastmail.com>
    Signed-off-by: ⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>

--- Formula/evry.rb
+++ Formula/evry.rb
@@ -2,7 +2,7 @@ class Evry < Formula
   desc "Shell-script-centric task scheduler"
   homepage "https://github.com/purarue/evry"
   url "https://github.com/purarue/evry/archive/5e3ebfbfb7a9236ab9fa7ef494fb19573d2095ca.tar.gz"
-  version "0.3.5,1,5e3ebfb"
+  version "0.3.5-1-5e3ebfb"
   sha256 "4223b0330bfe438d8fc547fa7ae9d7ea7610f3304091a84c6c132bff92011eab"
   license "Apache-2.0"
   head "https://github.com/purarue/evry.git", branch: "master"
