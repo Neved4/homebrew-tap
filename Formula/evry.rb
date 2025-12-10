79346b95d62616429a4bec2c683328fa3c7b7a8b
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2025-12-10 09:17:08 +0000

    evry: add 0.3.5-1-5e3ebfb bottle.

--- Formula/evry.rb
+++ Formula/evry.rb
@@ -11,6 +11,13 @@ class Evry < Formula
     skip "No tagged version available"
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/evry-0.3.5-1-5e3ebfb"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "deacfbc2c5b346a2154abdaa612dedc4f34572a4c0768a3f43ef2124667dd2cd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66b7bbf66b0154c3e3d48716d77e6decb1dd3d0a0d5595e1b3e8a2510ec09452"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d98dd2b204ef469ced004862b3e3910a630798ec69cbd15fef4c7d00dcf1a0b"
+  end
+
   depends_on "rust" => :build
 
   def install
