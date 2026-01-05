82e4ea659330d32063c72c3ff25f8043c27437d0
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-05 13:57:16 +0100

    Fix seclists audit
    Closes #355.
    
    Signed-off-by: ⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>

--- Formula/seclists.rb
+++ Formula/seclists.rb
@@ -2,7 +2,6 @@ class Seclists < Formula
   desc "Collection of multiple security wordlists"
   homepage "https://github.com/danielmiessler/SecLists"
   url "https://gitlab.com/kalilinux/packages/seclists/-/archive/upstream/2025.3/seclists-upstream-2025.3.tar.gz"
-  version "2025.3"
   sha256 "fa2c4abff3e1b039a10778ddde43e7bae06710310e3709cc45c9eedc98b377e8"
   license "MIT"
 
