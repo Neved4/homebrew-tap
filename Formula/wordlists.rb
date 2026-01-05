2d728f9a911cfbeac09bbdbd7c3ee62445e8eac2
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-01-05 22:51:44 +0000

    wordlists: add 2025.4.0 bottle.

--- Formula/wordlists.rb
+++ Formula/wordlists.rb
@@ -5,6 +5,13 @@ class Wordlists < Formula
   sha256 "bc30fa77ec18d1c6b25c4fce28664a2f4cc6d35fad5f026907fba0fac2ddc6e9"
   license :cannot_represent
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/wordlists-2025.4.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9d7faa3631950897b52de0a1ccd45cb7c5bc8b37626a1a7172769b6ea3043d30"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f01054a7e670280ee6053264a41203b34282a316ee341816af4a73c8d510f32c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1100e875698d31e57a254a0705e29fc958cec0c270108626fc1aa7e30394dcc0"
+  end
+
   def install
     pkgshare.install Dir["*"]
   end
