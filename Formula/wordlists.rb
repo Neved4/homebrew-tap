02540c2e6c24d13f70eb9a22b733ddf8fc7617e7
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-05 13:57:36 +0100

    Fix wordlists audit

--- Formula/wordlists.rb
+++ Formula/wordlists.rb
@@ -2,7 +2,6 @@ class Wordlists < Formula
   desc "Collection of common wordlists"
   homepage "https://www.kali.org/tools/wordlists/"
   url "https://gitlab.com/kalilinux/packages/wordlists/-/archive/kali/2025.4.0/wordlists-kali-2025.4.0.tar.gz"
-  version "2025.4.0"
   sha256 "bc30fa77ec18d1c6b25c4fce28664a2f4cc6d35fad5f026907fba0fac2ddc6e9"
   license :cannot_represent
 
