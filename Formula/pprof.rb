9627f579f9a9f5fe98958bf8ec414d3ff3e73fbb
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-05 17:48:32 +0100

    Add pprof formula
    Signed-off-by: Neved4 <63655535+Neved4@users.noreply.github.com>

---
+++ Formula/pprof.rb
@@ -0,0 +1,22 @@
+class Pprof < Formula
+  desc "Profiling tool for Go programs"
+  homepage "https://github.com/google/pprof"
+  url "https://github.com/google/pprof/archive/b05bdaca462fe6a8410d248bdc10c20e01a7b489.tar.gz"
+  version "0.0.0-20251213-b05bdac"
+  sha256 "e7c2ebf84a2df6ba109dbc019c24760dfa3c2c7274f0de5aad97bf561fe16375"
+  license "Apache-2.0"
+
+  livecheck do
+    skip "No tagged version available"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+  end
+
+  test do
+    assert_match "pprof", shell_output("#{bin}/pprof -h 2>&1")
+  end
+end
