4f19f213df529f08805e0554153c47b5b50b3875
risc <148492865+0risc@users.noreply.github.com>
2026-01-06 13:39:20 +0000

    seclists: add 2025.3-0kali1 bottle.

--- Formula/seclists.rb
+++ Formula/seclists.rb
@@ -6,6 +6,13 @@ class Seclists < Formula
   sha256 "73dd0fc383e5e1dab058e9e83b211126713bb2cdfbc1c601c90ac4cc375d1d3e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/seclists-2025.3-0kali1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d38b54ef82776124c96c4eb78cc0dbdb29796c7699ec28b2c6c7013a0df00a05"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b4ad8ffcf2aa0ef4b1a5c87610b7774b0831ee662e068c19dbef93a571725cc4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9db3b205e3d1112a99cf6b64c7836b4aefc7e71b87d9253ee3a61eb57bfd59e3"
+  end
+
   def install
     pkgshare.install Dir["*"]
   end
