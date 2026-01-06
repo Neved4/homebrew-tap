d99c34d27f5c8fb2ebc065e020fc94ecb41f2744
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-06 14:55:36 +0100

    seclists: add 2025.3 bottle

--- Formula/seclists.rb
+++ Formula/seclists.rb
@@ -6,6 +6,11 @@ class Seclists < Formula
   sha256 "73dd0fc383e5e1dab058e9e83b211126713bb2cdfbc1c601c90ac4cc375d1d3e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/seclists-2025.3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "7dabd3230bfca73cc276535b3dcf94c08d86fc96f05a27e44013aac481b87df1"
+  end
+
   def install
     pkgshare.install Dir["*"]
   end
