1f96a772e61803dad06f62d30c76e7f4afbbb7cd
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-01-07 19:03:09 +0000

    tidy: add 0.3.17 bottle.

--- Formula/tidy.rb
+++ Formula/tidy.rb
@@ -6,6 +6,13 @@ class Tidy < Formula
   license "MIT"
   head "https://github.com/sts10/tidy.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tidy-0.3.17"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9ce01c78622e50bb07a959bb5de8322f1323ab050753f785ccab1f4ac0e2ba6e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "27003244eb4ecfcc33c2b14c7a639cb5da60379cecc812980307a877efcccb7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "038b75ea954395d4b5c17ab6684b1207a32b42cf3f390b51717429ced3a37db8"
+  end
+
   keg_only "conflicts with wordtidy"
 
   depends_on "rust" => :build
