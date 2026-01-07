311a3f7ca306e6fb7fff42575dd67449f33657f2
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-01-07 19:03:29 +0000

    inferno: add 0.9.9 bottle.

--- Formula/inferno.rb
+++ Formula/inferno.rb
@@ -5,6 +5,13 @@ class Inferno < Formula
   sha256 "b0492a971f32e08e084b8065d656cec6c7d8c4dd7287c1c359612b3254658e5a"
   license "CDDL-1.0"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/inferno-0.9.9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b3765978a71babd6486017500c254b422c88f4714d84c964b7f552ff3b98d958"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e50c7a7c5343e453db46bd06fe6cb792b331a5e3bbc8ef338819aca039f0720"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3958a9216b9470348a4daf52dc4181744a8063daf1b29908e80fe7b16a08ab0c"
+  end
+
   depends_on "rust" => :build
 
   def install
