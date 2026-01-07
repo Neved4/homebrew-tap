0c59382e43dc183b71b9941fdbc8a3bc1af85f8c
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-01-07 19:12:13 +0000

    gochro: add 0.0.0-20251209-57ed4cd bottle.

--- Formula/gochro.rb
+++ Formula/gochro.rb
@@ -10,6 +10,13 @@ class Gochro < Formula
     skip "No tagged version available"
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/gochro-0.0.0-20251209-57ed4cd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e0754b1fa2b8931b3aa82a792ff7419d994cbfca2add43e4d5fef2cb5dd8b29e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a55cb70a76dca70b7e31801f7108b5c2ea7f003716089d505ef736640d04834"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d24360bfc3cb89030fa447acd6f896c7019c85820dd15fd26bd354c2bc154f22"
+  end
+
   depends_on "go" => :build
 
   def install
