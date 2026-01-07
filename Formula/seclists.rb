7b2e94e1c4fd877e998dc51d0e4a0a50e504012d
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-07 20:24:53 +0100

    Use upstream SecLists 2025.3 tarball

--- Formula/seclists.rb
+++ Formula/seclists.rb
@@ -1,15 +1,10 @@
 class Seclists < Formula
   desc "Collection of multiple security wordlists"
   homepage "https://github.com/danielmiessler/SecLists"
-  url "https://gitlab.com/kalilinux/packages/seclists/-/archive/upstream/2025.3-0kali2025.3/seclists-upstream-2025.3-0kali2025.3.tar.gz"
-  version "2025.3-0kali1"
-  sha256 "73dd0fc383e5e1dab058e9e83b211126713bb2cdfbc1c601c90ac4cc375d1d3e"
+  url "https://github.com/danielmiessler/SecLists/archive/refs/tags/2025.3.tar.gz"
+  version "2025.3"
+  sha256 "8fa88740c36777012f637ebb844de43d1020fb4273e080019d5f0c80360f433f"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/seclists-2025.3"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "1cf0bd513b40004f1a62c210bd8129a349893da87284b53c45eca5609b1b2bcb"
-  end
 
   def install
     pkgshare.install Dir["*"]
