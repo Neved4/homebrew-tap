5ad21bbb598a38bb93b8b65b5e99ab9d1b00c029
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-01-07 19:03:14 +0000

    astroguard: add 0.1-20240122-9ca1197 bottle.

--- Formula/astroguard.rb
+++ Formula/astroguard.rb
@@ -10,6 +10,13 @@ class Astroguard < Formula
     skip "No tagged version available"
   end
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/astroguard-0.1-20240122-9ca1197"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dcbce15c81419126e466eb6963687460e941dc82490a361062928dd05d34b6c4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1bd03e364f2aa2bf28c3e29a1d939f1b5e4817b445e8e16beaf50ebcce3ca41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5bc78aaa0072d5ebc63365c18f3c764c1f1f33f79bb4478ca242d7f780d1614e"
+  end
+
   depends_on "gcc"
 
   def install
