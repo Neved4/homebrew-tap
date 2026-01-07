3b783e088599acb1db424d8ad6f449f206a77ab2
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-07 20:18:54 +0100

    Restore seclists version

--- Formula/seclists.rb
+++ Formula/seclists.rb
@@ -2,6 +2,7 @@ class Seclists < Formula
   desc "Collection of multiple security wordlists"
   homepage "https://github.com/danielmiessler/SecLists"
   url "https://gitlab.com/kalilinux/packages/seclists/-/archive/upstream/2025.3-0kali2025.3/seclists-upstream-2025.3-0kali2025.3.tar.gz"
+  version "2025.3-0kali1"
   sha256 "73dd0fc383e5e1dab058e9e83b211126713bb2cdfbc1c601c90ac4cc375d1d3e"
   license "MIT"
 
