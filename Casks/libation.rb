da650c3368d8944b6d0db40c8488fda82ad74bd2
risc <risc@fastmail.com>
2026-01-18 09:06:52 +0000

    fix(cask): add workaround for brew bug on linux
    Signed-off-by: risc <risc@fastmail.com>

--- Casks/libation.rb
+++ Casks/libation.rb
@@ -3,7 +3,9 @@ cask "libation" do
 
   version "13.1.3"
   sha256 arm:   "4de5da9d042c638df8dd335ae257665876588fdfc76ef37bf4c31b057e92deea",
-         intel: "7a7058104567750e524cdebb2cc453d3b420cfc0dc8ef132f6e871511586fce8"
+         intel: "7a7058104567750e524cdebb2cc453d3b420cfc0dc8ef132f6e871511586fce8",
+         arm64_linux:  "0", # workaround for brew bug https://github.com/orgs/Homebrew/discussions/6008, https://github.com/Homebrew/homebrew-cask/issues/205491
+         x86_64_linux: "0"
 
   url "https://github.com/rmcrackan/Libation/releases/download/v#{version}/Libation.#{version}-macOS-chardonnay-#{arch}.dmg",
       verified: "github.com/rmcrackan/Libation/"
