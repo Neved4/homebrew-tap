67efc010eba8ef720b68d030dda0ce4f266cb953
risc <148492865+0risc@users.noreply.github.com>
2026-02-03 12:14:22 +0000

    tailsocks: add 1.2.0 bottle.

--- Formula/tailsocks.rb
+++ Formula/tailsocks.rb
@@ -6,6 +6,12 @@ class Tailsocks < Formula
   license "MIT"
   head "https://github.com/ItalyPaleAle/tailsocks.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tailsocks-1.2.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "97e2ec27bfeab4635362094f03a826012d5d4a5bf0953cde1baef1b6869c391e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6f6fd6b0d5c87304335b7644175cd3f3685611cc43805494e295ed4c57e58152"
+  end
+
   depends_on "go" => :build
 
   def install
