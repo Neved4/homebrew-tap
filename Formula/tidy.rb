1ae39d0166650f1816d172b4fca62d5516ea4b12
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-02-16 23:14:52 +0000

    tidy 0.3.24
    Closes #391.
    
    Signed-off-by: ⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>

--- Formula/tidy.rb
+++ Formula/tidy.rb
@@ -1,8 +1,8 @@
 class Tidy < Formula
   desc "Combine and clean word lists"
   homepage "https://github.com/sts10/tidy"
-  url "https://github.com/sts10/tidy/archive/refs/tags/v0.3.20.tar.gz"
-  sha256 "0ef5624acbe496724da65a03bffab0807d8926c2bd58cf988a5ac5da345a7c69"
+  url "https://github.com/sts10/tidy/archive/refs/tags/v0.3.24.tar.gz"
+  sha256 "dbe180e9a6ea97d472881dcba0059c19135ef2b110e13b1827a7a8e8772de113"
   license "MIT"
   head "https://github.com/sts10/tidy.git", branch: "main"
 
