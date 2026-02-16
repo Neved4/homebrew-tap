e458264276e35d02f49c71998f61dcda1a604666
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-02-16 23:26:20 +0000

    tidy: update 0.3.24 bottle.

--- Formula/tidy.rb
+++ Formula/tidy.rb
@@ -7,9 +7,9 @@ class Tidy < Formula
   head "https://github.com/sts10/tidy.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tidy-0.3.20"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "dff9e11630171d2475cbf85736cd951b0aa493155507e0d1c70ace3fa30cdbba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6bbaaaf1f3491ab53f24a2022128ce84a99911ff616e726129324aaf4934eecf"
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tidy-0.3.24"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "fa0c347c6912503c5a15e84c477784ee9b70693bbb257ea8c2884b0a0fb22794"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0150e5dea4247e8f72e9a7949a37c895932d6f7c143d7dcb19d82520d7fd5f09"
   end
 
   keg_only "conflicts with wordtidy"
