87f672f793ce64a5cb416f6e2a1056282ad0ab11
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-02-23 23:58:22 +0000

    tailsocks: update 1.2.2 bottle.

--- Formula/tailsocks.rb
+++ Formula/tailsocks.rb
@@ -7,9 +7,9 @@ class Tailsocks < Formula
   head "https://github.com/ItalyPaleAle/tailsocks.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tailsocks-1.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "97e2ec27bfeab4635362094f03a826012d5d4a5bf0953cde1baef1b6869c391e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6f6fd6b0d5c87304335b7644175cd3f3685611cc43805494e295ed4c57e58152"
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/tailsocks-1.2.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e123a730ad726ba6f6cd9c97863f6bdc35c4c278c452571743ff0bdd00f6e431"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "919e570749febcd0a08856b2a16d3cbb8468d91ccad29a15fa3419b19ff9590b"
   end
 
   depends_on "go" => :build
