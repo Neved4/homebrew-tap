efea73194a19423b38aba0bff5f44de63a68b598
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-02-24 08:00:58 +0000

    cinny 4.10.5

--- Casks/cinny.rb
+++ Casks/cinny.rb
@@ -1,6 +1,6 @@
 cask "cinny" do
-  version "4.10.2"
-  sha256 "3b95a300b6396e122af71674dd8f89458826b7890b427b17fd50031095876990"
+  version "4.10.5"
+  sha256 "aa06c9e6adde3b6b091c551b5348e6a762f321ed12bd26993d394d810576f690"
 
   url "https://github.com/cinnyapp/cinny-desktop/releases/download/v#{version}/Cinny_desktop-universal.app.tar.gz"
   name "cinny"
