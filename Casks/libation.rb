7c54b6515f68f4651bb759c79a0b33e6b267208e
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-03-05 23:23:24 +0100

    fix(cask): add linux sha256 placeholders for libation audit

--- Casks/libation.rb
+++ Casks/libation.rb
@@ -2,8 +2,10 @@ cask "libation" do
   arch arm: "arm64", intel: "x64"
 
   version "13.2.1"
-  sha256 arm:   "b05dd70fe6d97fb499164e9dd030d8e04c62e8050c3519bff23e873c318a37dd",
-         intel: "3d773194cf8ce1b2534379ac83a2f8d87d334b00ffb7f4e426305fe9470a3b8e"
+  sha256 arm:          "b05dd70fe6d97fb499164e9dd030d8e04c62e8050c3519bff23e873c318a37dd",
+         intel:        "3d773194cf8ce1b2534379ac83a2f8d87d334b00ffb7f4e426305fe9470a3b8e",
+         arm64_linux:  "0", # workaround for brew bug: Homebrew/homebrew-cask#205491
+         x86_64_linux: "0"
 
   url "https://github.com/rmcrackan/Libation/releases/download/v#{version}/Libation.#{version}-macOS-chardonnay-#{arch}.dmg",
       verified: "github.com/rmcrackan/Libation/"
