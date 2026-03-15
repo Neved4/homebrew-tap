2e4caa64feb76008e3ad10f2d7add524f5b9d1d6
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-03-15 13:54:51 +0000

    google-chrome-testing 146.0.7680.80

--- Casks/google-chrome-testing.rb
+++ Casks/google-chrome-testing.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-testing" do
   arch arm: "arm64", intel: "x64"
 
-  version "146.0.7680.72"
+  version "146.0.7680.80"
   sha256 :no_check
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
