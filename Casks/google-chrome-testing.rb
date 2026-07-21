e3babb44b26c66142e99304881332ec538422d55
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-07-21 14:52:16 +0000

    google-chrome-testing 151.0.7922.34
    Closes #464.
    
    Signed-off-by: ⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>

--- Casks/google-chrome-testing.rb
+++ Casks/google-chrome-testing.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-testing" do
   arch arm: "arm64", intel: "x64"
 
-  version "148.0.7778.167"
+  version "151.0.7922.34"
   sha256 :no_check
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chrome-mac-#{arch}.zip",
