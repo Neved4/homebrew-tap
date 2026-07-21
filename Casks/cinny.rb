1a6c0bf7af4fbb3e88fd515589c94b0f05784700
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-07-21 14:52:07 +0000

    cinny 4.12.5
    Closes #463.
    
    Signed-off-by: ⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>

--- Casks/cinny.rb
+++ Casks/cinny.rb
@@ -1,6 +1,6 @@
 cask "cinny" do
-  version "4.12.1"
-  sha256 "e09805c1df53e7859bef14f06d5bd2c97618515097994ebdc78ec6ba8fcb69a9"
+  version "4.12.5"
+  sha256 "6f1778475761512e9eab6b0ababe9f6d6cb25b16d97285393ebfa9ea30a84b68"
 
   url "https://github.com/cinnyapp/cinny-desktop/releases/download/v#{version}/Cinny_desktop-universal.app.tar.gz"
   name "cinny"
