7a163a37385c046b0f43b32fba2beebdf7bbd91d
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-07-21 14:48:07 +0000

    keyscan: update 0.0.0-20260507-c309a27 bottle.

--- Formula/keyscan.rb
+++ Formula/keyscan.rb
@@ -20,10 +20,9 @@ class Keyscan < Formula
   end
 
   bottle do
-    root_url "https://github.com/Neved4/homebrew-tap/releases/download/keyscan-0.0.0-20260101-c297a23"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a8ee10b405585452cfcf409c759909eb19093dca94c7d2d52f37aacda39db9b5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e725fb5f0d86c0338330861a9191e5f6ee17a18bd0fc537303d852197895be7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd4c4e68a8d811905e51a8ffe0fff651b06d7911bc396ba6da1c9762ec968e30"
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/keyscan-0.0.0-20260507-c309a27"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "c1093f5de5b0641788e5e3df8694e408a3da69a12751cc59c01a275b8cf55fae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "df73ff34582412d217d4335419d8a74df1fb03c689a915aefe1a095124b45791"
   end
 
   depends_on "python@3.13"
