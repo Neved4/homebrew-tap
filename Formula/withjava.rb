a959365052e0ae5bc3add1d0d80cde2b7ff51d22
⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>
2026-07-22 08:07:35 +0000

    withjava: add 0.1.1 bottle.

--- Formula/withjava.rb
+++ Formula/withjava.rb
@@ -5,6 +5,12 @@ class Withjava < Formula
   sha256 "dff33c4cb192c645bb7b44c724001033a34d4e5b88375e77a29a1c4400300b42"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/Neved4/homebrew-tap/releases/download/withjava-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "4637ab9dbc50bd1845985e7f2901ba2c3ae18ffbee0adad8dcef66c5161bb616"
+    sha256 cellar: :any,                 x86_64_linux: "b337df132795bd2c8a01ab16903a6c7824599158ee8056090d0a8ea326f63a7e"
+  end
+
   depends_on "rust" => :build
 
   def install
