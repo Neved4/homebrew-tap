067b162bcea603258dcfff4e0b15d533784f98bf
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-09-01 04:36:09 +0000

    puncia 0.37

--- Formula/puncia.rb
+++ Formula/puncia.rb
@@ -3,8 +3,8 @@ class Puncia < Formula
 
   desc "Official CLI utility for Osprey Vision, Subdomain Center & Exploit Observer"
   homepage "https://github.com/ARPSyndicate/puncia"
-  url "https://github.com/ARPSyndicate/puncia/archive/refs/tags/v0.34.tar.gz"
-  sha256 "10ba6994422a33433ec6cf057b8cf42afbadb94dab8d668098a44a8a96daa193"
+  url "https://github.com/ARPSyndicate/puncia/archive/refs/tags/v0.37.tar.gz"
+  sha256 "8468a8ecd4d29cc52bd8651c5b21d5f8da06964c880f4856d41231270450ee95"
   license "MIT"
   head "https://github.com/ARPSyndicate/puncia.git", branch: "master"
 
