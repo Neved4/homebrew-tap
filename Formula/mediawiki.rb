97e974249f1015dc86f00e9588923cf194c44407
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-09-06 22:17:03 +0200

    fix(mediawiki): correct install-step quoting
    Closes #486.
    
    Signed-off-by: ⑆ Neved4 ⑈ <63655535+Neved4@users.noreply.github.com>

--- Formula/mediawiki.rb
+++ Formula/mediawiki.rb
@@ -52,7 +52,7 @@ class Mediawiki < Formula
   post_install_steps do
     unless_path_exists "mediawiki/index.php", base: :var do
       mkdir_p "mediawiki", base: :var
-      run "/bin/sh", args: ["-c", "cp -R '{{pkgshare}}'/.' '{{var}}/mediawiki/'"]
+      run "/bin/sh", args: ["-c", "cp -R {{pkgshare}}/. {{var}}/mediawiki/"]
     end
   end
 
